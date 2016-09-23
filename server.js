226473aee447058e6264278238e96dee61401f4a
Spencer Charest <spencercharest@Spencer-MacBook-Pro.local>
2016-09-23 12:43:19 -0700

    static test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 //Sets up the Express app to serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app/public')));
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,6 +18,11 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 
+//routes
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'app/public/index.html'));
+});
 
 // Starts the server to begin listening
 // =============================================================
